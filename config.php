04d172ee76f958480647fa8b8a44bd19a6fe4c7f
John Puddephatt <john@jdp.org.uk>
2021-04-17 14:05:53 +0100

    first commit

---
+++ config.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Str;
+
+return [
+    'baseUrl' => '/docs',
+    'production' => false,
+    'siteName' => 'Community Wakefield Docs',
+    'siteDescription' => 'Help and advice on how to get the most out of Community Wakefield',
+
+    // Algolia DocSearch credentials
+    'docsearchApiKey' => '',
+    'docsearchIndexName' => '',
+
+    // navigation menu
+    'navigation' => require_once('navigation.php'),
+
+    // helpers
+    'isActive' => function ($page, $path) {
+        return Str::endsWith(trimPath($page->getPath()), trimPath($path));
+    },
+    'isActiveParent' => function ($page, $menuItem) {
+        if (is_object($menuItem) && $menuItem->children) {
+            return $menuItem->children->contains(function ($child) use ($page) {
+                return trimPath($page->getPath()) == trimPath($child);
+            });
+        }
+    },
+    'url' => function ($page, $path) {
+        return Str::startsWith($path, 'http') ? $path : '/' . trimPath($path);
+    },
+];
